e49ec025e77d0d107532fb117e5502344aea9de2
eagle1967 <icsourcery@gmail.com>
2013-01-10 12:09:09 -0700

    added flip and shake to snooze-Ronald Ramsay II  -has been taken from Artem AchepClock implementation for ICS deskclock source at http://github.com/Achep/android_packages_apps_DeskClock Originally Ported-by: Arnav Gupta <championswimmer@gmail.com>

--- src/com/android/deskclock/SettingsActivity.java
+++ src/com/android/deskclock/SettingsActivity.java
@@ -50,6 +50,10 @@ public class SettingsActivity extends PreferenceActivity
             "alarm_in_silent_mode";
     static final String KEY_ALARM_SNOOZE =
             "snooze_duration";
+    static final String KEY_FLIP_ACTION =
+	    "flip_action";
+    static final String KEY_SHAKE_ACTION =
+            "shake_action";
     static final String KEY_VOLUME_BEHAVIOR =
             "volume_button_setting";
     static final String KEY_AUTO_SILENCE =
@@ -175,6 +179,14 @@ public class SettingsActivity extends PreferenceActivity
             final ListPreference listPref = (ListPreference) pref;
             final int idx = listPref.findIndexOfValue((String) newValue);
             listPref.setSummary(listPref.getEntries()[idx]);
+        } else if (KEY_FLIP_ACTION.equals(pref.getKey())) {
+            final ListPreference listPref = (ListPreference) pref;
+            String action = (String) newValue;
+            updateFlipActionSummary(listPref, action);
+        } else if (KEY_SHAKE_ACTION.equals(pref.getKey())) {
+            final ListPreference listPref = (ListPreference) pref;
+            String action = (String) newValue;
+            updateShakeActionSummary(listPref, action);
         } else if (KEY_DIGITAL_CLOCK_TIME_COLOR.equals(pref.getKey())
                 || KEY_DIGITAL_CLOCK_DATE_COLOR.equals(pref.getKey())
                 || KEY_DIGITAL_CLOCK_ALARM_COLOR.equals(pref.getKey())) {
@@ -199,6 +211,21 @@ public class SettingsActivity extends PreferenceActivity
         }
     }
 
+    }
+
+    private void updateFlipActionSummary(ListPreference listPref, String action) {
+        int i = Integer.parseInt(action);
+        listPref.setSummary(getString(R.string.flip_action_summary,
+            getResources().getStringArray(R.array.flip_action_entries)[i]
+                .toLowerCase()));
+    }
+
+    private void updateShakeActionSummary(ListPreference listPref, String action) {
+        int i = Integer.parseInt(action);
+        listPref.setSummary(getString(R.string.shake_summary, getResources()
+            .getStringArray(R.array.flip_action_entries)[i].toLowerCase()));
+    }
+
     private void refresh() {
         ListPreference listPref = (ListPreference) findPreference(KEY_AUTO_SILENCE);
         String delay = listPref.getValue();
@@ -219,6 +246,16 @@ public class SettingsActivity extends PreferenceActivity
 
         listPref = (ListPreference) findPreference(KEY_VOLUME_BUTTONS);
         listPref.setSummary(listPref.getEntry());
+        listPref.setOnPreferenceChangeListener(this);
+
+        listPref = (ListPreference) findPreference(KEY_FLIP_ACTION);
+        String action = listPref.getValue();
+        updateFlipActionSummary(listPref, action);
+        listPref.setOnPreferenceChangeListener(this);
+
+        listPref = (ListPreference) findPreference(KEY_SHAKE_ACTION);
+        String shake = listPref.getValue();
+        updateShakeActionSummary(listPref, shake);
         listPref.setOnPreferenceChangeListener(this);
 
         ColorPickerPreference clockTimeColor = (ColorPickerPreference) findPreference(KEY_DIGITAL_CLOCK_TIME_COLOR);
